981721ad3bd50b6b7e72a6b2faffe5ea493c0e8d
hajiit <hajiit@inbox.ru>
2020-04-03 20:56:44 +0700

    rework chat.kt

--- app/src/main/java/ru/skillbranch/devintensive/models/Chat.kt
+++ app/src/main/java/ru/skillbranch/devintensive/models/Chat.kt
@@ -1,10 +1,14 @@
 package ru.skillbranch.devintensive.models
 
-class Chat(val id: String) {
-    companion object ChatFactory{
+class Chat(
+    val id: String,
+    val members: MutableList<User> = mutableListOf(),
+    val messages: MutableList<BaseMessage> = mutableListOf()
+) {
+    companion object ChatFactory {
         private var lastId = -1
         fun makeChat(): Chat {
-            lastId ++
+            lastId++
             return Chat("$lastId")
         }
     }
